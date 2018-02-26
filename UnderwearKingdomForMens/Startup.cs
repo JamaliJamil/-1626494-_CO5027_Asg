afcc98d5a789f0e02ac2c9dc2c7cea6decd56055
JamaliJamil <36616907+JamaliJamil@users.noreply.github.com>
2018-02-26 16:32:51 +0800

    Add project files.

---
+++ UnderwearKingdomForMens/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UnderwearKingdomForMens.Startup))]
+namespace UnderwearKingdomForMens
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
